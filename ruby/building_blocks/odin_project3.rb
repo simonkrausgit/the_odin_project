6512f5fa382c8413b0450fba2ee8124ddfe275e9
simonkrausgit <simon.kraus.usa@gmail.com>
2015-09-10 10:15:25 +0700

    substring method can return correct values when given a single word

--- ruby/building_blocks/odin_project3.rb
+++ ruby/building_blocks/odin_project3.rb
@@ -4,6 +4,20 @@
 #--------------------
 class OdinProject3
 
-  def initialize()
+  #def initialize()
+  #end
+
+  def substrings(string, dict)
+
+    #Create empty hash to be filled
+    return_hash = {}
+
+    #Cycle through every dict entry
+    dict.each do |dic|
+      if string.include? dic
+        return_hash.store(dic,1)
+      end
+    end
+    return_hash
   end
 end
--- ruby/building_blocks/spec/odin_project3_spec.rb
+++ ruby/building_blocks/spec/odin_project3_spec.rb
@@ -6,4 +6,16 @@ describe OdinProject3 do
     @odin = OdinProject3.new
   end
 
+  dictionary = [
+    "below","down","go","going","horn","how","howdy","it","i",
+    "low","own","part","partner","sit"]
+
+  describe '#substrings' do
+    it 'should return a hash' do
+      expect(@odin.substrings("test",dictionary)).to be_kind_of(Hash)
+    end
+    it 'should return the number of matches if given a single word' do
+      expect(@odin.substrings("below",dictionary)).to eq({"below"=>1, "low"=>1})
+    end
+  end
 end
