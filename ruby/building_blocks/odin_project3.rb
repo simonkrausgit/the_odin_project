6427a38faa0654ff2d60a9ede1d8b472f1d7513a
simonkrausgit <simon.kraus.usa@gmail.com>
2015-09-10 10:25:51 +0700

    substrings method works with a longer string now

--- ruby/building_blocks/odin_project3.rb
+++ ruby/building_blocks/odin_project3.rb
@@ -12,10 +12,18 @@ class OdinProject3
     #Create empty hash to be filled
     return_hash = {}
 
+    #downcase the string as we want to detect case insensitive
+    string_array = string.downcase.scan(/\w+/)
     #Cycle through every dict entry
     dict.each do |dic|
-      if string.include? dic
-        return_hash.store(dic,1)
+      string_array.each do |s|
+        if s.include? dic
+          if return_hash.has_key?(dic)
+            return_hash[dic] += 1
+          else
+            return_hash.store(dic,1)
+          end
+        end
       end
     end
     return_hash
--- ruby/building_blocks/spec/odin_project3_spec.rb
+++ ruby/building_blocks/spec/odin_project3_spec.rb
@@ -17,5 +17,10 @@ describe OdinProject3 do
     it 'should return the number of matches if given a single word' do
       expect(@odin.substrings("below",dictionary)).to eq({"below"=>1, "low"=>1})
     end
+    it 'should return the number of matches if given a larger string' do
+      expect(@odin.substrings(
+        "Howdy partner, sit down! How's it going?",dictionary)).
+          to eq({"down"=>1, "how"=>2, "howdy"=>1,"go"=>1, "going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,"partner"=>1,"sit"=>1})
+    end
   end
 end
