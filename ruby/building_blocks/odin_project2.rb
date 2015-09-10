53d6a5c4e6984fd90723cf38861322e12cff240c
simonkrausgit <simon.kraus.usa@gmail.com>
2015-09-10 09:58:56 +0700

    Cleaned up odin_project2 a little bit while adding some comments

--- ruby/building_blocks/odin_project2.rb
+++ ruby/building_blocks/odin_project2.rb
@@ -1,38 +1,39 @@
 # Part of the Odin Project Curriculum
-
+#---------------------
+#Project2
+#--------------------
 class OdinProject2
-  #---------------------
-  #Project2
-  #--------------------
-  def initialize()
 
+  def initialize()
     #0: profit, 1: buying day, 2: selling day
-   @profit = Array.new(3,0)
+    @profit = Array.new(3,0)
   end
 
+    #Pseudo-code for how the method goes through the array
+    # Array: 0 1 2 3 4
+    # 0-1 0-2 0-3 0-4
+    # 1-2 1-3 1-4
+    # 2-3 2-4
+    # 3-4
   def stock_picker(stocks = [])
-#Pseudo-code:
-# Array: 0 1 2 3 4
-# 0-1 0-2 0-3 0-4
-# 1-2 1-3 1-4
-# 2-3 2-4
-# 3-4
-
+    
+    #stocks_length is used more often inside the loop, so it's declared here
+    #the subtraction is necessary to not include the last element during looping
     stocks_length = stocks.length - 1
 
+    #Outer loop cycling through each element in the array except the last one
     (stocks_length).times do |i|
-      (stocks_length - i).times do |j|
 
+      #inner loop allowing for comparing the current element ...
+      #to all the remaining elements
+      (stocks_length - i).times do |j|
         future_day = stocks[j+i+1]
         today = stocks[i]
         if (future_day - today) > @profit[0]
           @profit = [future_day - today, i, j+i+1]
         end
-
       end
     end
-
     @profit
   end
-
 end
