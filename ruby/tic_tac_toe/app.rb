9bb66855e1dc7caabe8b28642ebfffaae4b57e60
simonkrausgit <simon.kraus.usa@gmail.com>
2015-09-11 23:55:36 +0700

    Started off Tic-Tac-Toe game

---
+++ ruby/tic_tac_toe/app.rb
@@ -0,0 +1,17 @@
+#
+# Tic Tac Toe implementatino as part of the Odin Project
+#
+class Game
+
+  initialize() do
+    @player1 = Player.new
+    @player2 = Player.new
+  end
+
+  class Player
+
+  end
+  class Board
+
+  end
+end
---
+++ ruby/tic_tac_toe/spec/app_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../app'
+
+describe "game" do
+  
+end
