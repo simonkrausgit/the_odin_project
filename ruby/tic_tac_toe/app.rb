3273cc065c0909cf5e3d7e8ba3e20f5f17d6b604
simonkrausgit <simon.kraus.usa@gmail.com>
2015-09-12 00:15:53 +0700

    Added initial draft of display function for the board

--- ruby/tic_tac_toe/app.rb
+++ ruby/tic_tac_toe/app.rb
@@ -3,15 +3,40 @@
 #
 class Game
 
-  initialize() do
+  initialize do
     @player1 = Player.new
     @player2 = Player.new
+    @board = Board.new
   end
 
   class Player
+    initialize do
 
+    end
   end
   class Board
 
+    initialize do
+      @fields =[["X","O",nil],["X",nil,"O"],[nil,"X",nil]]
+    end
+
+    display do
+      @fields.each do |row|
+        puts "-------"
+        row.each do |item|
+          string = "|"
+          if item != nil
+            string += ' |'
+          else
+            string += "#{item}|"
+          end
+          puts string
+        end
+        puts "-------"
+      end
+    end
+
   end
 end
+
+game = Game.new
