04375e4999136a9debeb64efb10619af422c28eb
simonkrausgit <simon.kraus.usa@gmail.com>
2015-09-13 23:24:33 +0700

    added basic functions for classes player, board and game with tests

--- ruby/tic_tac_toe/app.rb
+++ ruby/tic_tac_toe/app.rb
@@ -5,30 +5,81 @@ class Game
 
   def initialize
     @board = Board.new
-    #display
-    #game_cycle("X")
+    @player1 = Player.new("1")
+    @player2 = Player.new("2")
   end
 
-  def display
-    @board.display
+  def display_player_marks
+    puts "Player1: #{@player1.mark} Player2: #{@player2.mark}"
+  end
+
+  def display_board
+    puts @board.return_display_string
+  end
+
+  def populate_board (array)
+    @board.field = array
+  end
+  def start_new_game
+    populate_board([[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]])
+    @player1.mark = find_symbol(@player1.mark)
+    @player2.mark = find_symbol(@player2.mark)
+  end
+  def find_symbol(player_mark)
+    #accepted_symbol = false
+    #first_try = true
+    puts("Player#{player_mark}, choose your symbol (it needs to be a single character):")
+    $stdin.gets.chomp
+    #until (accepted_symbol)
+    #  system("clear")
+    #  puts("Sorry, your input is not supported.") if first_try
+    #  puts("Player#{player_mark}, choose your symbol (it needs to be a single character):")
+    #  symbol = gets.chomp
+    # =~ /\w/
+    #  first_try = false
+    #end
+  end
+  def cycle_through
+
   end
 
   class Board
 
+    attr_accessor :field
+
     def initialize
-      @field =[["X","O",nil],["X",nil,"O"],[nil,"X",nil]]
+      @field =[[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
+    end
+
+    def return_display_string
+      string = "\n  -------"
+      @field.each do |row|
+        string += "\n  |"
+        row.each do |item|
+          if item != nil
+            string += "#{item}|"
+          else
+            string += ' |'
+          end
+        end
+      end
+      string +="\n  -------"
     end
   end
 
   class Player
 
+    attr_accessor :mark
+
     def initialize(mark)
       @mark = mark
     end
+
+    def take_turn()
+
+    end
   end
 end
-
-
 
 =begin
 
--- ruby/tic_tac_toe/spec/app_spec.rb
+++ ruby/tic_tac_toe/spec/app_spec.rb
@@ -2,26 +2,85 @@ require_relative '../app'
 
 describe "Game" do
 
-  #before(:example) do
-  #  @game = Game.new
-  #end
+  before(:example) do
+    @game = Game.new
+  end
 
   it "should be of class Game" do
-    @game = Game.new
     expect(@game.class).to eq(Game)
   end
+
+  describe "#display_player_marks" do
+    it "should properly print the player markers" do
+      expect{@game.display_player_marks}.to output("Player1: 1 Player2: 2\n").to_stdout
+    end
+  end
+  describe "#display_board" do
+    it "should print the correct field data in the console" do
+      expect{@game.display_board}.to output("\n  -------\n  | | | |\n  | | | |\n  | | | |\n  -------\n").to_stdout
+      @game.populate_board([["X","O",nil],["X",nil,"O"],[nil,"X",nil]])
+      expect{@game.display_board}.to output("\n  -------\n  |X|O| |\n  |X| |O|\n  | |X| |\n  -------\n").to_stdout
+    end
+  end
+  describe "#find_symbol" do
+    it "should return the symbol that was typed in" do
+      allow(STDIN).to receive(:gets).and_return("a\n", "D\n")
+      expect(@game.find_symbol("1")). to eq("a")
+      expect(@game.find_symbol("1")). to eq("D")
+    end
+  end
+  describe "#start_new_game" do
+    it "should return the correct symbols that were typed in" do
+      #allow(@game.start_new_game).to receive(:get).and_return("a\n", "D\n")
+      allow(STDIN).to receive(:gets).and_return("a\n", "D\n")
+      @game.start_new_game
+      expect{@game.display_player_marks}.to output("Player1: a Player2: D\n").to_stdout
+    end
+  end
+
 end
 
 describe "Board" do
-  it "should be of class Board" do
+
+  before(:example) do
     @board = Game::Board.new
+  end
+
+  it "should be of class Board" do
     expect(@board.class).to eq(Game::Board)
   end
+
+  describe "#field" do
+    it "should fill up the field variable correctly" do
+      @board.field = ([["X" , "O" , nil],["O", nil , nil],["X", nil , "O"]])
+      expect(@board.field).to eq([["X" , "O" , nil],["O", nil , nil],["X", nil , "O"]])
+    end
+  end
+  describe "#return_display_string" do
+    it "should return the correct string with all empty fields" do
+      expect(@board.return_display_string).to eq("\n  -------\n  | | | |\n  | | | |\n  | | | |\n  -------")
+    end
+    it "should return the correct string using a filled board" do
+      @board.field = ([["X","O",nil],["X",nil,"O"],[nil,"X",nil]])
+      expect(@board.return_display_string).to eq("\n  -------\n  |X|O| |\n  |X| |O|\n  | |X| |\n  -------")
+    end
+  end
+
 end
 
 describe "Player" do
-  it "should be of class Player" do
+
+  before(:example) do
     @player = Game::Player.new("O")
+  end
+
+  it "should be of class Player" do
     expect(@player.class).to eq(Game::Player)
+  end
+  describe "#setting_and_reading" do
+    it "should correctly set and return the Player's mark" do
+      @player.mark = "X"
+      expect(@player.mark).to eq("X")
+    end
   end
 end
