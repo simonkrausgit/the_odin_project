8a83fbbf7aad7fbaf676052586f87d968a6d2125
simonkrausgit <simon.kraus.usa@gmail.com>
2015-09-13 21:02:58 +0700

    started rspec framework

--- ruby/tic_tac_toe/spec/app_spec.rb
+++ ruby/tic_tac_toe/spec/app_spec.rb
@@ -1,5 +1,27 @@
 require_relative '../app'
 
-describe "game" do
-  
+describe "Game" do
+
+  #before(:example) do
+  #  @game = Game.new
+  #end
+
+  it "should be of class Game" do
+    @game = Game.new
+    expect(@game.class).to eq(Game)
+  end
+end
+
+describe "Board" do
+  it "should be of class Board" do
+    @board = Game::Board.new
+    expect(@board.class).to eq(Game::Board)
+  end
+end
+
+describe "Player" do
+  it "should be of class Player" do
+    @player = Game::Player.new("O")
+    expect(@player.class).to eq(Game::Player)
+  end
 end
